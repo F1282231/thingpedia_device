d1f39f32036c3d4b5e57160f3295b350e04d7be6
Jack Wang <jackweijiawang@gmail.com>
2020-04-14 18:29:02 -0700

    Added com.fee

---
+++ test/com.fee.js
@@ -0,0 +1,9 @@
+// -*- mode: js; indent-tabs-mode: nil; js-basic-offset: 4 -*-
+//
+// Copyright 2020 Jack Wang <jackweijiawang@gmail.com>
+//
+// See LICENSE for details
+"use strict";
+
+module.exports = [
+];
