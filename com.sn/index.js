f1988e8e7898a33561968a826bffea9b312722f5
Jack Wang <jackweijiawang@gmail.com>
2020-04-14 18:34:43 -0700

    Added com.sn

---
+++ com.sn/index.js
@@ -0,0 +1,13 @@
+// -*- mode: js; indent-tabs-mode: nil; js-basic-offset: 4 -*-
+//
+// This file is part of com.sn
+//
+// Copyright 2020 Jack Wang <jackweijiawang@gmail.com>
+//
+// See LICENSE for details
+"use strict";
+
+const Tp = require('thingpedia');
+
+module.exports = class extends Tp.BaseDevice {
+};
